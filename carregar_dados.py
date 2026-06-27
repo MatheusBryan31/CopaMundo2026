54a1d0ffe6eb361a695e17f2a871a233fdb23192
MatheusBryan31 <BryanAlmeida56@hotmail.com>
2026-06-27 18:43:15 -0300

    Adição da biblioteca json no arquivo requirements.txt

--- carregar_dados.py
+++ carregar_dados.py
@@ -1,30 +1,26 @@
-import requests
+import json
+import os
 
 # =============================================================
 # ARQUIVO PARA CARREGAMENTO DOS DADOS
 # =============================================================
 
-# URL dos arquivos JSON no GitHub
+PASTA_DADOS = "Data"
+
 def carregar_dados():
-    anos = range(1930, 2027, 4)
     copas = []
+    anos = range(1930, 2027, 4)
 
     for ano in anos:
         if ano in [1942, 1946]:
             continue
-
-        URL_DADOS = f"https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/{ano}/worldcup.json"
-
-        dados = requests.get(URL_DADOS).json()
-
-        copas.append(dados)
-    return copas
+        with open(f"{PASTA_DADOS}/{ano}.json", encoding="utf-8") as f:
+            copas.append(json.load(f))
+        return copas
 
 if __name__ == "__main__":
-
     copas = carregar_dados()
-
     print(f"Foram carregadas {len(copas)} Copas.")
 
     for copa in copas:
-        print(copa['name'])
+        print(copa["name"])
