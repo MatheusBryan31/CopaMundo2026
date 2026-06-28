cc018521328fd1e84c47f878cb7a7712f316cb4c
MatheusBryan31 <BryanAlmeida56@hotmail.com>
2026-06-27 23:37:51 -0300

    Colocando uma arquitetura de código padrão no arquivo utils.py

--- api_paises.py
+++ api_paises.py
@@ -3,3 +3,10 @@ import requests
 # =============================================================
 # ARQUIVO PARA CONSULTAR DADOS DE UM PAÍS NA API
 # =============================================================
+def consultar_pais(nome_pais):
+    url = f"https://restcountries.com/v3.1/name/{nome_pais}"
+    resposta = requests.get(url)
+
+    if resposta.status_code == 200:
+        return resposta.json()[0]
+    return None
